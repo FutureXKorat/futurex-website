f599f0586b4beb94cb94e037129c588546a74b32
sevendecha <sevendecha@gmail.com>
2026-06-23 22:04:47 +0800

    test resend.com

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
-echo "hello world";
-exit;
+
+$resend = Resend::client('re_esV8qrib_3L1LaajT2gXjHxC55FSWXSSz');
+
+$resend->emails->send([
+  'from' => 'onboarding@resend.dev',
+  'to' => 'futurexkorat@gmail.com',
+  'subject' => 'Hello World',
+  'html' => '<p>Congrats on sending your <strong>first email</strong>!</p>'
+]);
