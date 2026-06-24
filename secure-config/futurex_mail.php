9d9a41f94c5eed2af9845688ccac4d3b6a6ee1a3
sevendecha <sevendecha@gmail.com>
2026-06-24 19:12:14 +0800

    Added Resend's email address.
    And removed 'admin_email'

--- secure-config/futurex_mail.php
+++ secure-config/futurex_mail.php
@@ -3,9 +3,8 @@ declare(strict_types=1);
 
 return [
     'RESEND_API_KEY' => 're_bqwS1JDr_3kpGuhYb4mzQBoQXJJnUcJGo',          // ← paste your real API key here
-    'FROM_EMAIL'     => 'onboarding@resend.dev', // ← temporary until domain is verified
+    'FROM_EMAIL'     => 'noreply@futurexthailand.com', // ← temporary until domain is verified
     'FROM_NAME_OTP'   => 'Future X OTP',
     'FROM_NAME_RESET' => 'Future X Reset Email',
     'FROM_NAME_ORDER' => 'Future X Order Form',
-    'ADMIN_EMAIL'     => 'futurexkorat@gmail.com',
 ];
